c85a28bc9b411fd168bfb0c9119615a8da4fb3ee
trymulyani <104083493+trymulyani@users.noreply.github.com>
2022-04-20 21:31:46 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,30 @@
+# if else 
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+Nama= input("Siapa nama kamu?")
+
+# program sebelumnya
+# if kondisi : aksi
+# program selanjutnya 
+
+if Nama==" Ojan" : Print("Kamu lucu sekali ojan")
+print(f"Terima kasih {Nama}")
+# print(f"Terima kasih {Nama}")
+
+# 2. program if indentation 
+if Nama==" Ojannn":
+    print("kamu lucu sekali Ojannn")
+    print("Kamu tidak lucu sam sekali Ojannn")
+print(f"Terima kasih {Nama}")
+
+#3. Else Statemant
+
+if Nama=="Raka":
+    print("Haiii Raka, kamu ganteng sekali")
+else :
+    print("Haii kamu bukan Raka, jangan so ganteng: (")
+
+print("Akhir dari program")
